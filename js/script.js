213658859d04df32fa6af77f731124d3f3349e8c
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-11 17:32:27 +0200

    cambiato modo per accedere true

--- js/script.js
+++ js/script.js
@@ -2,26 +2,28 @@
 const emailCorrette = ["spiderman@gmail.com", "batman@gmail.com", "hulk@gmail.com", "pellegrino@gmail.com", "ironman@gmail.com"];
 
 let messaggio = document.querySelector('.messaggio');
-let email = document.querySelector('.email');
+
 let btn = document.getElementById("login");
 
 btn.addEventListener("click", function () {
 
     let emailUtente = document.getElementById("mail").value;
+    let risulta = false;
 
     for (i = 0; i < emailCorrette.length; i++) {
         console.log(emailUtente);
 
-        if (emailUtente == emailCorrette[i]) {
-            messaggio.innerHTML = "Loggato";
-
-            break;
+        if (emailUtente === emailCorrette[i]) {
+            risulta = true;
         }
         else {
-            messaggio.innerHTML = "Email non valida - Inserisci un email valida";
-            
         }
+    } if (risulta) {
+        messaggio.innerHTML = "Loggato";
+    } else {
+        messaggio.innerHTML = "Email non valida - Inserisci un email valida";
     }
+
 
 })
 
